c01f1de367742a20ed5e06d6ddc036b4a62b89a5
Hong-ji-min <jmh907@naver.com>
2024-07-02 15:37:45 +0900

    Feat : Dockerfile 추가, V0.1.0

--- src/main/java/com/sh/app/AppController.java
+++ src/main/java/com/sh/app/AppController.java
@@ -1,5 +1,6 @@
 package com.sh.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.message.AsynchronouslyFormattable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class AppController {
 
     @Value("${app.version}")
@@ -15,6 +17,7 @@ public class AppController {
     @GetMapping("/")
     @ResponseBody // 리턴한 객체를 바로 응답 메세지에 써주는 역할
     public String index() {
+        log.info("GET / : appVersion = {}", appVersion);
         return """
                 <h1>🙈Welcome to Hello World Application🙉</h1>
                 <h4>version %s</h4>
